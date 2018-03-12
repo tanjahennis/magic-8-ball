6efcd2074afecd9dbc70e8dac5de6438dd836188
Tanja Hennis <tmhennis@gmail.com>
2018-03-12 16:30:36 +0100

    app done

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by tanja on 12/03/2018.
+//  Copyright © 2018 tanja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomBallNumber : Int = 0
+    
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        newBallImage()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func askButtonPressed(_ sender: Any) {
+        newBallImage()
+    }
+
+    func newBallImage() {
+        
+        randomBallNumber = Int(arc4random_uniform(4))
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
+        
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        newBallImage()
+    }
+}
+
